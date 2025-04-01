7c836500a131874fba2e945686ff274a16bf12dd
kylindai <kylin@hotmail.com>
2025-04-02 00:25:31 +0800

    for test

--- web/app_wss/app/job/test_job.py
+++ web/app_wss/app/job/test_job.py
@@ -7,3 +7,7 @@ from sanic.log import logger
 def task_test(task_id, job_id):
     logger.info("task_test ...")
     # await asyncio.sleep(1)
+
+
+async def async_task_test(task_id, job_id):
+    logger.info("async_task_test ...")
