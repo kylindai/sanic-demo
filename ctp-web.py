14bc0a9f260497a0416e59c9b0ff992211590c92
kylindai <kylin@hotmail.com>
2025-04-05 21:32:55 +0800

    for test

--- ctp-web.py
+++ ctp-web.py
@@ -6,6 +6,7 @@ from sanic import Sanic
 from sanic.worker.loader import AppLoader
 from web.app_wss.main import create_app
 
+# app = create_app()
 
 if __name__ == '__main__':
     loader = AppLoader(factory=partial(create_app))
@@ -14,5 +15,6 @@ if __name__ == '__main__':
     Sanic.serve(primary=app, app_loader=loader)
 
     # app.run(host='0.0.0.0', port=8001, debug=True, auto_reload=False)
+    
     # uvicorn.run(app=app, host='0.0.0.0', port=8001)
     # uvicorn.run("web.app_wss.main:create_app", host='0.0.0.0', port=8001, workers=2, reload=False)
